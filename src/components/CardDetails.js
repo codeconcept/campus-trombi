c222fae5ec8d46dec3b4ef0f1101f1df63160a56
codeconcept <contact@codeconcept.fr>
2020-02-03 18:42:44 +0100

    Checkbox for pictures only

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,27 +1,36 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
 import CardModal from "./CardModal";
-export default function CardDetails({ card }) {
+export default function CardDetails({ card, imageOnly }) {
   return (
     <>
-      <Card>
-        <Image src={card.pictureUrl} />
-        <Card.Content>
-          <Card.Header>
-            {card.name} {card.firstName}
-          </Card.Header>
-          <Card.Meta>
-            <span className="date">{card.class}</span>
-          </Card.Meta>
-          <Card.Description>{card.cellPhone}</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <span>{card.inCampus ? "sur le campus" : "absent"}</span>{" "}
-          <span style={{ cursor: "pointer" }}>
-            <CardModal student={card} />
-          </span>
-        </Card.Content>
-      </Card>
+      {imageOnly ? (
+        <Card>
+          {" "}
+          <Card.Content>
+            <Image src={card.pictureUrl} />
+          </Card.Content>
+        </Card>
+      ) : (
+        <Card>
+          <Image src={card.pictureUrl} />
+          <Card.Content>
+            <Card.Header>
+              {card.name} {card.firstName}
+            </Card.Header>
+            <Card.Meta>
+              <span className="date">{card.class}</span>
+            </Card.Meta>
+            <Card.Description>{card.cellPhone}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <span>{card.inCampus ? "sur le campus" : "absent"}</span>{" "}
+            <span style={{ cursor: "pointer" }}>
+              <CardModal student={card} />
+            </span>
+          </Card.Content>
+        </Card>
+      )}
     </>
   );
 }
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,23 +1,31 @@
-import React from "react";
-import { Grid } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Grid, Checkbox } from "semantic-ui-react";
 import CardDetails from "./CardDetails";
 
 export default function CardList({ cards }) {
+  const [imageOnly, setImageOnly] = useState(false);
+
+  const handleChange = (e, data) => {
+    console.log(data.checked);
+    setImageOnly(data.checked);
+  };
   return (
     <>
+      <br />
+      <Checkbox toggle onChange={handleChange} label="photos uniquement" />
       <Grid columns={3} doubling stackable>
         {cards.map(card => (
           <Grid.Column
             key={card.id}
             style={{
-              width: 390,
+              width: 330,
               border: "2 solid red",
               float: "left",
               padding: 15,
               margin: 15
             }}
           >
-            <CardDetails card={card} />
+            <CardDetails card={card} imageOnly={imageOnly} />
           </Grid.Column>
         ))}
       </Grid>
