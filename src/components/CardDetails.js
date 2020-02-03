2c9f435d7eb19c11dc041c706b721b30ae215ffd
codeconcept <contact@codeconcept.fr>
2020-02-03 17:19:00 +0100

    Modal to display user details

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
-
+import CardModal from "./CardModal";
 export default function CardDetails({ card }) {
   return (
     <>
@@ -16,7 +16,10 @@ export default function CardDetails({ card }) {
           <Card.Description>{card.cellPhone}</Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <span>{card.inCampus ? "sur le campus" : "absent"}</span>
+          <span>{card.inCampus ? "sur le campus" : "absent"}</span>{" "}
+          <span style={{ cursor: "pointer" }}>
+            <CardModal student={card} />
+          </span>
         </Card.Content>
       </Card>
     </>
---
+++ src/components/CardModal.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Header, Image, Modal, Icon } from "semantic-ui-react";
+
+const CardModal = ({ student }) => (
+  <Modal trigger={<Icon name="male" />} centered={false}>
+    <Modal.Header>Fiche détaillée</Modal.Header>
+    <Modal.Content image>
+      <Image wrapped size="medium" src={student.pictureUrl} />
+      <Modal.Description>
+        <Header>
+          {student.firstName} {student.name.toUpperCase()}
+        </Header>
+        <p>classe : {student.class}</p>
+        <p>tél. : {student.cellPhone}</p>
+        <p>mail : {student.email}</p>
+      </Modal.Description>
+    </Modal.Content>
+  </Modal>
+);
+
+export default CardModal;
